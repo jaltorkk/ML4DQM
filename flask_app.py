f8211d4f41df0607243231006a99bec8b5631015
jaltorkk <jawaher.altork@cern.ch>
2024-05-16 17:09:52 +0200

    update

--- flask_app.py
+++ flask_app.py
@@ -14,11 +14,21 @@ def result():
     test_run_list = request.form['test_run_list']
 
     # Write the run list files
-    with open('static/runlist_2d_phieta.py', 'w') as f:
-        f.write("trainingrunlist = " + str(training_run_list) + "\n")
-        f.write("testrunlist = " + str(test_run_list) + "\n")
-        f.write("test_MET_tail = False\n")
-        f.write("test_2D_phivseta = True\n")
+    #with open('runlist_2d_phieta.py', 'w') as f:
+    #    f.write("trainingrunlist = " + str(training_run_list) + "\n")
+    #    f.write("testrunlist = " + str(test_run_list) + "\n")
+     #   f.write("test_MET_tail = False\n")
+     #   f.write("test_2D_phivseta = True\n")
+    
+    try:
+        with open('static/runlist_2d_phieta.py', 'w') as f:
+            f.write("trainingrunlist = " + str(training_run_list) + "\n")
+            f.write("testrunlist = " + str(test_run_list) + "\n")
+            f.write("test_MET_tail = False\n")
+            f.write("test_2D_phivseta = True\n")
+    except Exception as e:
+        app.logger.error(f"Error writing run list files: {e}")
+        return "An error occurred while writing run list files.", 500
 
     # Execute the script
     subprocess.run(["python", "ae_2d_phieta.py"], check=True)
