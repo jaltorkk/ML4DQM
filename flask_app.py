50cc78498614e4b9f9e1bca0c083b7e8bed2ea24
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-05-16 14:30:59 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -19,7 +19,7 @@ def result():
     return render_template('result.html', images=images)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0',port=8001)
     
 
 
