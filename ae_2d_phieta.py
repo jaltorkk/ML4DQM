266a1b459358d2c5d56f69ee38d44d757c782035
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-06-30 21:09:49 +0200

    Update ae_2d_phieta.py

--- ae_2d_phieta.py
+++ ae_2d_phieta.py
@@ -9,13 +9,13 @@ class RunLocations:
 
 run_locations = RunLocations()
 
-#def process_runs(training_run_list_str, test_run_list_str):
+def process_runs(training_run_list_str, test_run_list_str):
     # Convert the string of runs entered in the web app to a list
-#    training_run_list = training_run_list_str.split(',')
-#    test_run_list = test_run_list_str.split(',')
+    training_run_list = training_run_list_str.split(',')
+    test_run_list = test_run_list_str.split(',')
     # Remove any leading or trailing spaces from each run
-#    training_run_list = [run.strip() for run in training_run_list]
-#    test_run_list = [run.strip() for run in test_run_list]
+    training_run_list = [run.strip() for run in training_run_list]
+    test_run_list = [run.strip() for run in test_run_list]
 
     # Lists contain PhiVSEta bins for each run 
 #    training_runs = []
