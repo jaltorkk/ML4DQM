e603be2ec09dd967e291500ac6ccd939a597fed4
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-07-01 14:18:38 +0200

    Update ae_2d_phieta.py

--- ae_2d_phieta.py
+++ ae_2d_phieta.py
@@ -32,168 +32,133 @@ def process_runs(training_run_list_str, test_run_list_str):
     test_run_list = [run.strip() for run in test_run_list]
     return training_run_list, test_run_list
 
-# Lists contain PhiVSEta bins for each run 
-training_runs = []
-test_runs = []
-training_lists = []
-test_lists = []
-norm_list_phieta_train = []
-norm_list_phieta_test = []
+def load_data(training_run_list, test_run_list):
+    training_runs = []
+    test_runs = []
+    training_lists = []
+    test_lists = []
+    norm_list_phieta_train = []
+    norm_list_phieta_test = []
 
-with open(run_locations.list_location, "r") as file:
-    for line in file:
-        run_number = line.strip()[14:20]
-        filelocation_22 = run_locations.get_file_path(run_number)
-        filelocation_2 = filelocation_22[0]
-        run_num2 = run_number
+    with open(run_locations.list_location, "r") as file:
+        for line in file:
+            run_number = line.strip()[14:20]
+            filelocation_2 = run_locations.get_file_path(run_number)[0]
 
-        if run_num2 in training_run_list:
-            file = TFile.Open(filelocation_2, "READ")
-            training_runs.append(run_num2)
-            file.cd(f"DQMData/Run {run_num2}/JetMET/Run summary/Jet/Cleanedak4PFJetsCHS")
-            phi_eta = gROOT.FindObject("PhiVSEta")
-            phi_eta_entr = phi_eta.GetEntries()
-            lists_phieta_train = []
-            max_bin_cont = 0.
+            if run_number in training_run_list:
+                file = TFile.Open(filelocation_2, "READ")
+                training_runs.append(run_number)
+                file.cd(f"DQMData/Run {run_number}/JetMET/Run summary/Jet/Cleanedak4PFJetsCHS")
+                phi_eta = gROOT.FindObject("PhiVSEta")
+                phi_eta_entr = phi_eta.GetEntries()
+                lists_phieta_train = []
+                max_bin_cont = 0.
 
-            for i in range(53, 1300):
-                j = i - 52
-                k = i - 51
-                if (k % 52 != 0) and (j % 52 != 0):
-                    bin_cont_phieta = phi_eta.GetBinContent(i)
-                    if bin_cont_phieta > max_bin_cont:
-                        max_bin_cont = bin_cont_phieta
-                    b_phieta = bin_cont_phieta / phi_eta_entr
-                    lists_phieta_train.append(b_phieta)
-                
-            training_lists.append(lists_phieta_train)
-            b_phieta_norm = max_bin_cont / phi_eta_entr
-            norm_list_phieta_train.append(b_phieta_norm)
+                for i in range(53, 1300):
+                    j = i - 52
+                    k = i - 51
+                    if (k % 52 != 0) and (j % 52 != 0):
+                        bin_cont_phieta = phi_eta.GetBinContent(i)
+                        if bin_cont_phieta > max_bin_cont:
+                            max_bin_cont = bin_cont_phieta
+                        b_phieta = bin_cont_phieta / phi_eta_entr
+                        lists_phieta_train.append(b_phieta)
 
-        if run_num2 in test_run_list:
-            file = TFile.Open(filelocation_2, "READ")
-            test_runs.append(run_num2)
-            file.cd(f"DQMData/Run {run_num2}/JetMET/Run summary/Jet/Cleanedak4PFJetsCHS")
-            phi_eta = gROOT.FindObject("PhiVSEta")
-            phi_eta_entr = phi_eta.GetEntries()
-            lists_phieta_test = []
-            max_bin_cont = 0.
+                training_lists.append(lists_phieta_train)
+                b_phieta_norm = max_bin_cont / phi_eta_entr
+                norm_list_phieta_train.append(b_phieta_norm)
 
-            for i in range(53, 1300):
-                j = i - 52
-                k = i - 51
-                if (k % 52 != 0) and (j % 52 != 0):
-                    bin_cont_phieta = phi_eta.GetBinContent(i)
-                    if bin_cont_phieta > max_bin_cont:
-                        max_bin_cont = bin_cont_phieta
-                    b_phieta = bin_cont_phieta / phi_eta_entr
-                    lists_phieta_test.append(b_phieta)
+            if run_number in test_run_list:
+                file = TFile.Open(filelocation_2, "READ")
+                test_runs.append(run_number)
+                file.cd(f"DQMData/Run {run_number}/JetMET/Run summary/Jet/Cleanedak4PFJetsCHS")
+                phi_eta = gROOT.FindObject("PhiVSEta")
+                phi_eta_entr = phi_eta.GetEntries()
+                lists_phieta_test = []
+                max_bin_cont = 0.
 
-            test_lists.append(lists_phieta_test)
-            b_phieta_norm = max_bin_cont / phi_eta_entr
-            norm_list_phieta_test.append(b_phieta_norm)
+                for i in range(53, 1300):
+                    j = i - 52
+                    k = i - 51
+                    if (k % 52 != 0) and (j % 52 != 0):
+                        bin_cont_phieta = phi_eta.GetBinContent(i)
+                        if bin_cont_phieta > max_bin_cont:
+                            max_bin_cont = bin_cont_phieta
+                        b_phieta = bin_cont_phieta / phi_eta_entr
+                        lists_phieta_test.append(b_phieta)
 
-return training_runs, test_runs, training_lists, test_lists, norm_list_phieta_train, norm_list_phieta_test
+                test_lists.append(lists_phieta_test)
+                b_phieta_norm = max_bin_cont / phi_eta_entr
+                norm_list_phieta_test.append(b_phieta_norm)
 
-# Normalize training and test runs
-training_lists = np.random.rand(10, 24 * 50)
-test_lists = np.random.rand(5, 24 * 50)
-norm_list_phieta_train = np.random.rand(10)
-norm_list_phieta_test = np.random.rand(5)
-training_runs = [f"run_{i}" for i in range(10)]
-test_runs = [f"test_run_{i}" for i in range(5)]
+    return training_runs, test_runs, training_lists, test_lists, norm_list_phieta_train, norm_list_phieta_test
 
-# Normalize training and test runs
-max_train=max(norm_list_phieta_train)
-max_test=max(norm_list_phieta_test)
-training_list=np.array(training_lists)  
-test_list=np.array(test_lists)
-training_list=training_list/max_train 
-test_list=test_list/max_test
-    
-#--------------------------Define a Structure of an Autoencoder-----------------------------
-# Encoder structure
-n_encoder1 = 1024
-n_encoder2 = 512
-n_encoder3 = 256
-n_encoder4 = 128
-n_encoder5 = 64
-# latent
-n_latent = 2
-# Decoder structure
-n_decoder5 = 64
-n_decoder4 = 128
-n_decoder3 = 256
-n_decoder2 = 512
-n_decoder1 = 1024
+def normalize_data(training_lists, test_lists, norm_list_phieta_train, norm_list_phieta_test):
+    max_train = max(norm_list_phieta_train)
+    max_test = max(norm_list_phieta_test)
+    training_list = np.array(training_lists)
+    test_list = np.array(test_lists)
+    training_list = training_list / max_train
+    test_list = test_list / max_test
+    return training_list, test_list
 
-reg= MLPRegressor(hidden_layer_sizes = (n_encoder1, n_encoder2, n_encoder3, n_encoder4, n_encoder5, n_latent, 
-                   n_decoder5, n_decoder4, n_decoder3, n_decoder2, n_decoder1), 
-                   activation = 'tanh', 
-                   solver = 'adam', 
-                   learning_rate_init = 0.001, 
-                   max_iter = 100, 
-                   tol = 0.0001, 
-                   verbose = False)
-reg.fit(training_list, training_list)
+def train_autoencoder(training_list):
+    reg = MLPRegressor(hidden_layer_sizes=(1024, 512, 256, 128, 64, 2, 64, 128, 256, 512, 1024),
+                       activation='tanh', solver='adam', learning_rate_init=0.001, max_iter=100, tol=0.0001)
+    reg.fit(training_list, training_list)
+    return reg
 
-#------------------------------------- loss maps ------------------------------------------
-# --------------------------------- Phi VS Eta----------------------------------------------
-# Training runs
-x_pred_train = reg.predict(training_list)
-x_pred_train = np.array(x_pred_train)
-# Loss map
-loss_map_tr=(training_list-x_pred_train)**2
-loss_map_tr=np.array(loss_map_tr)
-loss_map_train=[]
-for i in range(len(loss_map_tr)):
-    img = loss_map_tr[i].reshape(24,50)
-    loss_map_train.append(img)
+def generate_loss_maps(reg, training_list, test_list, output_folder, training_runs, test_runs):
+    os.makedirs(output_folder, exist_ok=True)
 
-# Test runs
-x_pred_test = reg.predict(test_list)
-x_pred_test = np.array(x_pred_test)
-# Loss map
-loss_map_te=(test_list-x_pred_test)**2
-loss_map_te=np.array(loss_map_te)
-loss_map_test=[]
-for i in range(len(loss_map_te)):
-    img = loss_map_te[i].reshape(24,50)
-    loss_map_test.append(img)   
+    x_pred_train = reg.predict(training_list)
+    loss_map_train = (training_list - x_pred_train) ** 2
+    x_pred_test = reg.predict(test_list)
+    loss_map_test = (test_list - x_pred_test) ** 2
 
-# --------------------------------------testing training data for PhiVSEta--------------------------------------
-# Reshape the training and test data to 24X50 to draw histogram
-img_train=[]
-for i in range(len(training_list)):
-    img = training_list[i].reshape(24,50)
-    img_train.append(img)
-img_test=[]
-for i in range(len(test_list)):
-    img = test_list[i].reshape(24,50)
-    img_test.append(img)
+    for idx in range(len(training_list)):
+        hist_name_tr3 = f"hist_phieta_tr3_{idx}"
+        canv_name_tr3 = f"c_phieta_tr3_{idx}"
+        hist_phieta_tr3 = TH2F(hist_name_tr3, hist_name_tr3, 50, -5, 5, 24, -3, 3)
+        for i in range(24):
+            phi = -3.125 + (0.25 * (i + 1))
+            for j in range(50):
+                eta = -5.1 + (0.2 * (j + 1))
+                hist_phieta_tr3.Fill(eta, phi, loss_map_train[idx][i * 50 + j])
+        c_phieta_tr3 = TCanvas(canv_name_tr3, canv_name_tr3, 200, 10, 700, 500)
+        hist_phieta_tr3.SetTitle(f"Training Run {training_runs[idx]} (Loss Map) ; #eta ; #phi ")
+        gStyle.SetPalette(55)
+        c_phieta_tr3.Draw()
+        hist_phieta_tr3.Draw("colz")
+        hist_phieta_tr3.SetStats(0)
+        max_z = (np.max(loss_map_train) + (np.max(loss_map_train) / 3))
+        hist_phieta_tr3.GetZaxis().SetRangeUser(0, max_z)
+        file_name_tr3 = os.path.join(output_folder, f'phieta_train_lossmap_{training_runs[idx]}.png')
+        c_phieta_tr3.SaveAs(file_name_tr3)
 
-# ----------------- Loss Map histogram (MSE) -----------------
-hist_loss_forall=TH1F("hist_loss_forall","hist_loss_forall",50,0,0.5)
-for idx in range(len(training_runs)):
-    hist_name_tr3 = f"hist_phieta_tr3_{idx}"
-    canv_name_tr3 = f"c_phieta_tr3_{idx}"
-    hist_phieta_tr3 = {}
-    c_phieta_tr3 = {}
-    file_name_tr3 = {}  
-    hist_phieta_tr3[idx]=TH2F(hist_name_tr3,hist_name_tr3,50,-5,5,24,-3,3)
-    for i in range(0,24):
-        phi=-3.125+(0.25*(i+1))
-        for j in range(0,50):
-            eta=-5.1+(0.2*(j+1))
-            hist_phieta_tr3[idx].Fill(eta,phi,loss_map_train[idx][i][j])
-            hist_loss_forall.Fill(loss_map_train[idx][i][j])
-    c_phieta_tr3[idx] = TCanvas( canv_name_tr3, canv_name_tr3, 200, 10, 700, 500)
-    hist_phieta_tr3[idx].SetTitle("Training Run " + training_runs[idx] +  " (Loss Map) ; #eta ; #phi ")
-    gStyle.SetPalette(55)
-    c_phieta_tr3[idx].Draw()
-    hist_phieta_tr3[idx].Draw("colz")
-    hist_phieta_tr3[idx].SetStats(0)
-    max_z=(np.max(loss_map_train)+(np.max(loss_map_train)/3))
-    hist_phieta_tr3[idx].GetZaxis().SetRangeUser(0,max_z)
-    file_name_tr3[idx] = f"static/phieta_train_lossmap_{training_runs[idx]}.png"
-    c_phieta_tr3[idx].SaveAs(file_name_tr3[idx])         
+    for idx in range(len(test_list)):
+        hist_name_te3 = f"hist_phieta_te3_{idx}"
+        canv_name_te3 = f"c_phieta_te3_{idx}"
+        hist_phieta_te3 = TH2F(hist_name_te3, hist_name_te3, 50, -5, 5, 24, -3, 3)
+        for i in range(24):
+            phi = -3.125 + (0.25 * (i + 1))
+            for j in range(50):
+                eta = -5.1 + (0.2 * (j + 1))
+                hist_phieta_te3.Fill(eta, phi, loss_map_test[idx][i * 50 + j])
+        c_phieta_te3 = TCanvas(canv_name_te3, canv_name_te3, 200, 10, 700, 500)
+        hist_phieta_te3.SetTitle(f"Test Run {test_runs[idx]} (Loss Map) ; #eta ; #phi ")
+        gStyle.SetPalette(55)
+        c_phieta_te3.Draw()
+        hist_phieta_te3.Draw("colz")
+        hist_phieta_te3.SetStats(0)
+        max_z = (np.max(loss_map_train) + (np.max(loss_map_train) / 3))
+        hist_phieta_te3.GetZaxis().SetRangeUser(0, max_z)
+        file_name_te3 = os.path.join(output_folder, f'phieta_test_lossmap_{test_runs[idx]}.png')
+        c_phieta_te3.SaveAs(file_name_te3)
+
+def run_analysis(training_run_list_str, test_run_list_str):
+    training_run_list, test_run_list = process_runs(training_run_list_str, test_run_list_str)
+    training_runs, test_runs, training_lists, test_lists, norm_list_phieta_train, norm_list_phieta_test = load_data(training_run_list, test_run_list)
+    training_list, test_list = normalize_data(training_lists, test_lists, norm_list_phieta_train, norm_list_phieta_test)
+    reg = train_autoencoder(training_list)
+    generate_loss_maps(reg, training_list, test_list, 'static/images', training_runs, test_runs)
