adafd36176349c1d640a076bac4b9d3e638d8c03
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-07-02 10:51:56 +0200

    Update run_locations.py

--- run_locations.py
+++ run_locations.py
@@ -43,7 +43,7 @@ def get_file_path(run_number):
 
 def make_txt():
     # Parse command-line arguments
-    training_run_list, test_run_list = process_runs()
+    training_run_list, test_run_list = process_runs(training_run_list_str, test_run_list_str)
 
     # File location
     filelocation = " "
