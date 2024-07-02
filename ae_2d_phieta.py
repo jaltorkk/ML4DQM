72c60e46cf46bfdcfa82634118fbe3ddf5b2dcf4
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-07-02 14:18:04 +0200

    Update ae_2d_phieta.py

--- ae_2d_phieta.py
+++ ae_2d_phieta.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-class RunLocations:
-    list_location = 'path/to/your/list_location.txt'
-    def get_file_path(self, run_number):
-        return [f"mock_path_to_file/Run{run_number}.root"]
-
-run_locations = RunLocations()
-
 def process_runs(training_run_list_str, test_run_list_str):
     # Convert the string of runs entered in the web app to a list
     training_run_list = training_run_list_str.split(',')
@@ -15,5 +8,10 @@ def process_runs(training_run_list_str, test_run_list_str):
     training_run_list = [run.strip() for run in training_run_list]
     test_run_list = [run.strip() for run in test_run_list]
     return training_run_list, test_run_list
+
+def make_txt():
+    with open("stat.txt","wr") as file:
+        file.write("------------hello------------------")
+        
 
 
