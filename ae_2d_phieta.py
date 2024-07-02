8f1df4de01a9e219fc7a1aed9e92782fed04d422
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-07-02 14:23:03 +0200

    Update ae_2d_phieta.py

--- ae_2d_phieta.py
+++ ae_2d_phieta.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+def make_txt():
+    with open("static/stat.txt","wr") as file:
+        file.write("------------hello------------------")
+
 def process_runs(training_run_list_str, test_run_list_str):
     # Convert the string of runs entered in the web app to a list
     training_run_list = training_run_list_str.split(',')
@@ -7,11 +11,8 @@ def process_runs(training_run_list_str, test_run_list_str):
     # Remove any leading or trailing spaces from each run
     training_run_list = [run.strip() for run in training_run_list]
     test_run_list = [run.strip() for run in test_run_list]
+    make_txt()
     return training_run_list, test_run_list
-
-def make_txt():
-    with open("stat.txt","wr") as file:
-        file.write("------------hello------------------")
         
 
 
