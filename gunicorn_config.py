75094c61ddfa7fdc9c57f2b01f0ec2c114d9d966
Jawaher Altork <151660414+jaltorkk@users.noreply.github.com>
2024-09-23 10:21:53 +0200

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '4'))
+
+threads = int(os.environ.get('GUNICORN_THREADS', '8'))
+
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '600'))
+
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8001')
+
+
+
+forwarded_allow_ips = '*'
+
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
